fbe584901ea2ce33b3e02bb5e9a37f4b0d303e97
Scott Waitukaitis <swaitukaitis@gmail.com>
2015-11-16 14:24:41 +0100

    Finished v1.0 of the drawCutout.py code

--- drawCutout.py
+++ drawCutout.py
@@ -3,12 +3,29 @@ import matplotlib.pyplot as plt
 import os
 import pickle as p
 
-datafile = '/Users/swaitukaitis/work/stampFolding/tempData.py'
-[D, g, l ] = p.load( open( datafile, 'rb' ) )
+#load the temporary data file saved by the freeCAD code stamp.py
+#datafile = '/Users/swaitukaitis/work/stampFolding/tempData.py'
+#[D, g, l ] = p.load( open( datafile, 'rb' ) )
+D = 50.0 / 25.4
+boltD = 3 / 25.4
 
-fig = plt.figure(figsize = [5,5], frameon = False )
+#set up the figure
+fig = plt.figure(figsize = [D*1.01,D*1.01], frameon = False )
 ax = fig.add_axes([0,0,1,1], aspect = 'equal')
+ax.set_xlim(-D/2.*1.01, D/2.*1.01)
+ax.set_ylim(-D/2.*1.01, D/2.*1.01)
+plt.axis('off')
 
-ax.plot(np.linspace(-2.5, 2.5, 100), np.linspace(-1, 1, 100), 'r')
+#draw the cutout boundary
+edgeCircle=plt.Circle((0,0),D/2.,facecolor='None', edgecolor = 'r', linewidth = 0.25)
+fig.gca().add_artist(edgeCircle)
 
+#draw the bolt holes
+boltCircle1 = plt.Circle((-D/4.,0), boltD/2., facecolor='None', edgecolor = 'r', linewidth = 0.25)
+boltCircle2 = plt.Circle((D/4.,0), boltD/2., facecolor='None', edgecolor = 'r', linewidth = 0.25)
+fig.gca().add_artist(boltCircle1)
+fig.gca().add_artist(boltCircle2)
+
+#save the figure as a pdf (so it can be read by the laser cutter)
+plt.savefig('testFig.pdf', facecolor = 'None', edgecolor = 'None', pad_inches=0)
 plt.show(fig)
